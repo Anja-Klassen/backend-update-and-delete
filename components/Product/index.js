8eac28ab5b4d4eda3cb061cb4bf37078ed26958f
Anja Klaßen <anja.klassen@posteo.de>
2024-01-26 12:32:32 +0100

    add PUT route & refactor ProductForm & send PUT request

--- components/Product/index.js
+++ components/Product/index.js
@@ -3,30 +3,69 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  console.log(isEditMode);
+  const { data: product, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  if (!data) {
+  if (!product) {
     return;
   }
 
   return (
     <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
+      <h2>{product.name}</h2>
+      <p>Description: {product.description}</p>
       <p>
-        Price: {data.price} {data.currency}
+        Price: {product.price} {product.currency}
       </p>
-      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit Product
+      </button>
+      {isEditMode === true ? (
+        <ProductForm onSubmit={handleEditProduct} isEditMode={isEditMode} />
+      ) : null}
+      {product.reviews.length > 0 && <Comments reviews={product.reviews} />}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
 }
+// - Create a state called `isEditMode` and initialize it with `false`.
+// - In the return statement, add a `<button>` with
+//   - `type="button"`,
+//   - `onClick={() => { setIsEditMode(!isEditMode); }}`,
+//   - and a proper text.
+// - In the return statement, display the `ProductForm` component depending on the `isEditMode` state (Hint: `isEditMode && ...`).
+// - pass our `handleEditProduct` function to the `ProductForm` as the `onSubmit` prop.
